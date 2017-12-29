b086c17e4234aa83bf5a700d6590b81dc5c602fd
jrotty <bssf@qq.com>
2017-12-29 18:43:10 +0800

    升级1.3
    解决对超链接地址也统计的问题，同时加入标签数量统计

--- Plugin.php
+++ Plugin.php
@@ -4,13 +4,13 @@
  *
  * @package <font color="red">WordCount</font>
  * @author Jrotty
- * @version 1.1.5
+ * @version 1.3
  * @link http://qqdie.com
  */
 class WordCount_Plugin implements Typecho_Plugin_Interface
 {
     /**
-     * 
+     * 插件版本号
      * @var string
      *
      *
@@ -31,7 +31,10 @@ public static function num(){
 		?><style>
 .field.is-grouped{display:-webkit-box;display:-ms-flexbox;display:flex;-webkit-box-pack:start;-ms-flex-pack:start;justify-content:flex-start;  -ms-flex-wrap: wrap;
   flex-wrap: wrap;}.field.is-grouped>.control{-ms-flex-negative:0;flex-shrink:0}.field.is-grouped>.control:not(:last-child){margin-bottom:.5rem;margin-right:.75rem}.field.is-grouped>.control.is-expanded{-webkit-box-flex:1;-ms-flex-positive:1;flex-grow:1;-ms-flex-negative:1;flex-shrink:1}.field.is-grouped.is-grouped-centered{-webkit-box-pack:center;-ms-flex-pack:center;justify-content:center}.field.is-grouped.is-grouped-right{-webkit-box-pack:end;-ms-flex-pack:end;justify-content:flex-end}.field.is-grouped.is-grouped-multiline{-ms-flex-wrap:wrap;flex-wrap:wrap}.field.is-grouped.is-grouped-multiline>.control:last-child,.field.is-grouped.is-grouped-multiline>.control:not(:last-child){margin-bottom:.75rem}.field.is-grouped.is-grouped-multiline:last-child{margin-bottom:-.75rem}.field.is-grouped.is-grouped-multiline:not(:last-child){margin-bottom:0}.tags{-webkit-box-align:center;-ms-flex-align:center;align-items:center;display:-webkit-box;display:-ms-flexbox;display:flex;-ms-flex-wrap:wrap;flex-wrap:wrap;-webkit-box-pack:start;-ms-flex-pack:start;justify-content:flex-start}.tags .tag{margin-bottom:.5rem}.tags .tag:not(:last-child){margin-right:.5rem}.tags:last-child{margin-bottom:-.5rem}.tags:not(:last-child){margin-bottom:1rem}.tags.has-addons .tag{margin-right:0}.tags.has-addons .tag:not(:first-child){border-bottom-left-radius:0;border-top-left-radius:0}.tags.has-addons .tag:not(:last-child){border-bottom-right-radius:0;border-top-right-radius:0}.tag{-webkit-box-align:center;-ms-flex-align:center;align-items:center;background-color:#f5f5f5;border-radius:3px;color:#4a4a4a;display:-webkit-inline-box;display:-ms-inline-flexbox;display:inline-flex;font-size:.75rem;height:2em;-webkit-box-pack:center;-ms-flex-pack:center;justify-content:center;line-height:1.5;padding-left:.75em;padding-right:.75em;white-space:nowrap}.tag .delete{margin-left:.25em;margin-right:-.375em}.tag.is-white{background-color:#fff;color:#0a0a0a}.tag.is-black{background-color:#0a0a0a;color:#fff}.tag.is-light{background-color:#fff;color:#363636}.tag.is-dark{background-color:#363636;color:#f5f5f5}.tag.is-primary{background-color:#00d1b2;color:#fff}.tag.is-info{background-color:#3273dc;color:#fff}.tag.is-success{background-color:#23d160;color:#fff}.tag.is-warning{background-color:#ffdd57;color:rgba(0,0,0,.7)}.tag.is-danger{background-color:#ff3860;color:#fff}.tag.is-large{font-size:1.25rem}.tag.is-delete{margin-left:1px;padding:0;position:relative;width:2em}.tag.is-delete:after,.tag.is-delete:before{background-color:currentColor;content:"";display:block;left:50%;position:absolute;top:50%;-webkit-transform:translateX(-50%) translateY(-50%) rotate(45deg);transform:translateX(-50%) translateY(-50%) rotate(45deg);-webkit-transform-origin:center center;transform-origin:center center}.tag.is-delete:before{height:1px;width:50%}.tag.is-delete:after{height:50%;width:1px}.tag.is-delete:focus,.tag.is-delete:hover{background-color:#e8e8e8}.tag.is-delete:active{background-color:#dbdbdb}.tag.is-rounded{border-radius:290486px}
-</style>
+.tag.is-tags {
+  background-color: #fa74d1;
+  color: #fff;
+}</style>
 
 <script language="javascript">
     var EventUtil = function() {};
@@ -45,15 +48,15 @@ public static function num(){
             obj['on' + EventType] = Handler;
         }
     }
-    if (document.getElementById("text")) {
-        EventUtil.addEventHandler(document.getElementById('text'), 'propertychange', CountChineseCharacters);
-        EventUtil.addEventHandler(document.getElementById('text'), 'input', CountChineseCharacters);
+    if (document.querySelector('#wmd-preview')) {
+        EventUtil.addEventHandler(document.querySelector('#wmd-preview'), 'propertychange', CountChineseCharacters);
+        EventUtil.addEventHandler(document.querySelector('#wmd-preview'), 'input', CountChineseCharacters);
     }
     function showit(Word) {
         alert(Word);
     }
     function CountChineseCharacters() {
-        Words = document.getElementById('text').value;
+        Words = document.querySelector('#wmd-preview').textContent;
         var W = new Object();
         var Result = new Array();
         var iNumwords = 0;
@@ -67,6 +70,7 @@ public static function num(){
       var znum = 0;
       var gl = 0;
       var paichu = 0;
+     
         for (i = 0; i < Words.length; i++) {
             var c = Words.charAt(i);
             if (c.match(/[\u4e00-\u9fa5]/) || c.match(/[\u0800-\u4e00]/) || c.match(/[\uac00-\ud7ff]/)) {
@@ -106,12 +110,20 @@ public static function num(){
         document.getElementById('zimu').innerText = znum;
         document.getElementById('shuzi').innerText = inum;
         document.getElementById("zifu").innerHTML = iTotal * 2 + (sTotal - iTotal) * 2 + eTotal;
+
     }
 </script>
 <script> 
+  function tagsnum(){ 
+  var biaoqian = 0;
+biaoqian = $(".token-input-list").find("li").length;
+document.getElementById("biaoqian").innerHTML = biaoqian - 1;
+setTimeout('tagsnum()', 200); 
+} 
 $(document).ready(function(){
-$("#wmd-editarea").after('<div class="field is-grouped"><span class="tag">共计：</span><div class="control"><div class="tags has-addons"><span class="tag is-dark" id="zishu">0</span> <span class="tag is-primary">个字数</span></div></div><div class="control"><div class="tags has-addons"><span class="tag is-dark" id="zifu">0</span> <span class="tag is-primary">个字符</span></div></div><span class="tag">包含：</span><div class="control"><div class="tags has-addons"><span class="tag is-light" id="hanzi">0</span> <span class="tag is-danger">个文字</span></div></div><div class="control"><div class="tags has-addons"><span class="tag is-light" id="biaodian">0</span> <span class="tag is-info">个符号</span></div></div><div class="control"><div class="tags has-addons"><span class="tag is-light" id="zimu">0</span> <span class="tag is-success">个字母</span></div></div><div class="control"><div class="tags has-addons"><span class="tag is-light" id="shuzi">0</span> <span class="tag is-warning">个数字</span></div></div></div>');
+$("#wmd-editarea").after('<div class="field is-grouped"><span class="tag">共计：</span><div class="control"><div class="tags has-addons"><span class="tag is-dark" id="zishu">0</span> <span class="tag is-primary">个字数</span></div></div><div class="control"><div class="tags has-addons"><span class="tag is-dark" id="zifu">0</span> <span class="tag is-primary">个字符</span></div></div><span class="tag">包含：</span><div class="control"><div class="tags has-addons"><span class="tag is-light" id="hanzi">0</span> <span class="tag is-danger">个文字</span></div></div><div class="control"><div class="tags has-addons"><span class="tag is-light" id="biaodian">0</span> <span class="tag is-info">个符号</span></div></div><div class="control"><div class="tags has-addons"><span class="tag is-light" id="zimu">0</span> <span class="tag is-success">个字母</span></div></div><div class="control"><div class="tags has-addons"><span class="tag is-light" id="shuzi">0</span> <span class="tag is-warning">个数字</span></div></div><div class="control"><div class="tags has-addons"><span class="tag is-light" id="biaoqian">0</span> <span class="tag is-tags">个标签</span></div></div></div>');
 CountChineseCharacters();
+tagsnum();
 });
 </script>
 <?php
